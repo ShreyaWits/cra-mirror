257b86da6b50391aefa726eb6f831bc1367e0eea
Parampreet Singh Rai <parampreets.rai@thewitslab.com>
2025-04-21 16:55:48 +0530

    feat(api): Add request validation and token authentication
    Adds middleware for validating requests and tokens.
    
    - Implements token validation middleware for authentication.
    - Implements request validation middleware using structs.
    - Applies middleware to encrypt, decrypt and generate-edek routes.

--- encryption_service/internal/app/routes/routes.go
+++ encryption_service/internal/app/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	commomiddleware "encryption_microservice/internal/common/middleware"
+	"encryption_microservice/internal/modules/encryption/api/dtos"
 	"encryption_microservice/internal/modules/encryption/api/handlers"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,7 +15,7 @@ func SetupAPIRoutes(app *fiber.App, handler handlers.EncryptionHandler) {
 
 	// Encryption routes
 	api := app.Group("/encryption")
-	api.Post("/encrypt", handler.HandleEncrypt)
-	api.Post("/decrypt", handler.HandleDecrypt)
-	api.Post("/generate-edek", handler.HandleGenerateEDEK)
+	api.Post("/encrypt", commomiddleware.TokenValidator(), commomiddleware.ValidateParams[dtos.EncryptRequest](), handler.HandleEncrypt)
+	api.Post("/decrypt", commomiddleware.TokenValidator(), commomiddleware.ValidateParams[dtos.DecryptRequest](), handler.HandleDecrypt)
+	api.Get("/generate-edek", commomiddleware.TokenValidator(), handler.HandleGenerateEDEK)
 }
---
+++ encryption_service/internal/common/middleware/validator.go
@@ -0,0 +1,90 @@
+package commomiddleware
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+// ValidateParams is a middleware that validates the request body, query parameters, and path parameters against a given struct T.
+//
+// It first tries to parse the request body, query parameters, and path parameters into the given struct T using the respective parser functions.
+// If any of the parsing fails, it returns a 400 Bad Request response with a JSON body containing a field error response with the appropriate error message.
+//
+// If all parsing succeeds, it then uses the go-playground/validator library to validate the struct T.
+// If the validation fails, it returns a 400 Bad Request response with a JSON body containing a field error response with the appropriate error message for each validation error.
+// If the validation succeeds, it stores the validated struct T in the context and continues to the next handler in the chain.
+//
+// The type parameter T must be a struct with fields that have validation tags.
+// The struct must also have a no-argument constructor, so that the middleware can create a new instance of the struct to validate.
+func ValidateParams[T any]() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var dto T
+
+		// Parse request body
+		if err := c.BodyParser(&dto); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+			// return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			// 	"fieldErrors": []dtos.FieldErrorResponseDTO{
+			// 		{
+			// 			Field:        "requestBody",
+			// 			ErrorMessage: "Invalid request body",
+			// 			ErrorCode:    "InvalidRequestBody",
+			// 		},
+			// 	},
+			// })
+		}
+
+		// Extract query parameters
+		if err := c.QueryParser(&dto); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+			// return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			// 	"fieldErrors": []dtos.FieldErrorResponseDTO{
+			// 		{
+			// 			Field:        "requestParams",
+			// 			ErrorMessage: "Failed to read request params",
+			// 			ErrorCode:    "InvalidRequestParams",
+			// 		},
+			// 	},
+			// })
+		}
+
+		// Extract path parameters
+		if err := c.ParamsParser(&dto); err != nil {
+			// return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			// 	"fieldErrors": []dtos.FieldErrorResponseDTO{
+			// 		{
+			// 			Field:        "requestParams",
+			// 			ErrorMessage: "Failed to read request params",
+			// 			ErrorCode:    "InvalidRequestParams",
+			// 		},
+			// 	},
+			// })
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
+		var validate = validator.New()
+		// Validate the DTO
+		if err := validate.Struct(dto); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+			// var errorMessages []dtos.FieldErrorResponseDTO
+
+			// for _, err := range err.(validator.ValidationErrors) {
+			// 	errorMessages = append(errorMessages, dtos.FieldErrorResponseDTO{
+			// 		Field:        err.Field(),
+			// 		ErrorMessage: fmt.Sprintf("Validation failed: '%s'", err.Tag()),
+			// 	})
+			// }
+
+			// if len(errorMessages) > 0 {
+			// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			// 		"fieldErrors": errorMessages,
+			// 	})
+			// }
+		}
+
+		// Store the validated DTO in the context
+		c.Locals("contextData", &dto)
+
+		// Continue to the next handler
+		return c.Next()
+	}
+}
