ce8765205023d2a209d111e154e91d12d1ba729e
Shreya Sankhwar <shreya.sankhwar@thewitslab.com>
2025-05-07 17:20:47 +0530

    feat:env setup

--- third_party_service/cmd/server/main.go
+++ third_party_service/cmd/server/main.go
@@ -1,14 +1,11 @@
 package main
 
+func main() {
 
-func main(){
+	// Load config
+	//  modules.RegisterModules()
 
-	 // Load config
-	 modules.RegisterModules()
-
-	 kafkaBrokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
-	 go thirdpartyservices.StartKafkaConsumer(kafkaBrokers)
- 
-	 select {} // keep process alive
+	//  kafkaBrokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
+	//  go thirdpartyservices.StartKafkaConsumer(kafkaBrokers)
 
 }
