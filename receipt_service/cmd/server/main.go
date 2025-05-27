15d7a07e68c25ca60d62e08dbf1d7dc0b9a2ffee
Eekesh Thapa <Eekesh@Eekesh-J061TW2QPC.local>
2025-05-27 17:48:18 +0530

    config and cache service implemented in receipt service

--- receipt_service/cmd/server/main.go
+++ receipt_service/cmd/server/main.go
@@ -1,16 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"nps-reciept-service/internal/utils"
-	"nps-reciept-service/internal/utils/bootstrap"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"nps-reciept-service/internal/config"
+	cacheclient "nps-reciept-service/internal/config"
+	"nps-reciept-service/internal/utils"
+	"nps-reciept-service/internal/utils/bootstrap"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if os.Getenv("IS_DOCKER") != "true" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("error loading environment variables: %v\n", err)
+		}
+	}
 
+	// Load environment variables
+	token := os.Getenv("JWT_TOKEN")
+	if token == "" {
+		log.Fatalf("JWT_TOKEN is missing in .env")
+	}
+
+	rawData, err := config.LoadConfigFromAPI(token)
+	if err != nil {
+		log.Fatalf("Failed to fetch config from API: %v", err)
+	}
+
+	cfg, err := config.NewConfig(rawData)
+	if err != nil {
+		log.Fatalf("Config validation failed: %v", err)
+	}
+	// Create cache client
+	cacheClient, err := cacheclient.NewRedisClient(cfg.CacheUrl)
+
+	if err != nil {
+		fmt.Println("failed to create cache client: %w", err)
+	}
+
+	if cacheClient == nil {
+		fmt.Println("cache client is nil despite no error")
+	}
+	// Set initial config
+	config.SetCurrentConfig(cfg)
+
+	// Set up config webhook endpoint
+	http.HandleFunc("/config/webhook", config.HandleConfigWebhook)
+	go func() {
+		if err := http.ListenAndServe(":8081", nil); err != nil {
+			log.Printf("Webhook server error: %v", err)
+		}
+	}()
+
+	// Bootstrap the services
 	server := bootstrap.BootstrapServices()
 
 	// Handle graceful shutdown
--- receipt_service/go.mod
+++ receipt_service/go.mod
@@ -5,6 +5,7 @@ go 1.24.2
 require (
 	github.com/alicebob/miniredis/v2 v2.34.0
 	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/golang/mock v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/redis/go-redis/v9 v9.7.3
 	github.com/sirupsen/logrus v1.9.3
